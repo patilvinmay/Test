ef7271f9022b87c95bd9ef0fc7c6f5a6dc77464b
Vinmay Patil <patilvinmay@gmail.com>
2021-08-24 17:35:23 +0530

    Add files via upload

---
+++ Open_From_Diff_Directory.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    sys.path.insert(1, 'C:\\Users\\admin\\Desktop')
+
+from Hello import print_hi
+
+print_hi('Vinu')
---
+++ Python_From_Git.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import requests
+import os
+
+url = 'https://raw.githubusercontent.com/patilvinmay/Test/main/Hello.py'
+req = requests.get(url)
+pycode = req.text
+print('\n')
+os.getcwd()
+
+f = open("temp.py", "w")
+f.write(pycode)
+f.close()
+
+from temp import print_hi
+print_hi('Vinmay')
