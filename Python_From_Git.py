553a1e5ad843897c4918d31d8a46ab438b3135c8
Vinmay Patil <patilvinmay@gmail.com>
2021-08-24 22:28:12 +0530

    Update Python_From_Git.py

--- Python_From_Git.py
+++ Python_From_Git.py
@@ -5,12 +5,14 @@ import os
 url = 'https://raw.githubusercontent.com/patilvinmay/Test/main/Hello.py'
 req = requests.get(url)
 pycode = req.text
-print('\n')
-os.getcwd()
+pwd = os.getcwd()
+pwd = pwd + '\\'
+print(pwd)
 
-f = open("temp.py", "w")
+f = open(pwd + "temp.py", "w")
 f.write(pycode)
 f.close()
 
 from temp import print_hi
+
 print_hi('Vinmay')
