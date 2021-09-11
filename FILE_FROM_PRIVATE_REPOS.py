ebb6405fbb2ce1fbb1bf3936ccf7db58dd091dd7
Vinmay Patil <patilvinmay@gmail.com>
2021-09-11 13:09:19 +0530

    Create FILE_FROM_PRIVATE_REPOS.py

---
+++ FILE_FROM_PRIVATE_REPOS.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+
+user = 'patilvinmay'
+pao = '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'
+
+github_session = requests.Session()
+github_session.auth = (user, pao)
+
+# providing raw url to download csv from github
+file_url = 'https://raw.githubusercontent.com/patilvinmay/Testing/main/Hello.py'
+
+download = github_session.get(file_url).text
+
+pwd="C:\\Users\\admin\\Desktop\\temp.py"
+
+f = open(pwd, "w")
+f.write(download)
+f.close()
+
+from temp import *
+
+print_hi("Vinmay")
