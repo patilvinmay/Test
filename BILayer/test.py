3e607a16c789eb0d28f5428f120865a219aadca4
Vinmay Patil <patilvinmay@gmail.com>
2023-10-04 17:31:58 +0530

    Update test.py

--- BILayer/test.py
+++ BILayer/test.py
@@ -1,4 +1,20 @@
-SQL_QUERY="SELECT * FROM <your_table> LIMIT 10;"
+import pandas as pd
+from sqlalchemy import create_engine
 
-# Connect to Snowflake and execute the SQL query
-snowsql -a "$SNOWSQL_ACCOUNT" -u "$SNOWSQL_USER" -p "$SNOWSQL_PASSWORD" -w "$SNOWSQL_WAREHOUSE" -d "$SNOWSQL_DATABASE" -s "$SNOWSQL_SCHEMA" -q "$SQL_QUERY"
+# Sample DataFrame
+data = {
+    'id': [1, 2, 3],
+    'name': ['Alice', 'Bob', 'Charlie']
+}
+
+df = pd.DataFrame(data)
+
+# Replace placeholders with your PostgreSQL database credentials and connection details
+DATABASE_URI = "postgresql://username:password@hostname:port/database_name"
+engine = create_engine(DATABASE_URI)
+
+# Define the table name
+table_name = 'your_table_name'
+
+# Write the DataFrame to the PostgreSQL table
+df.to_sql(table_name, engine, if_exists='replace', index=False)
