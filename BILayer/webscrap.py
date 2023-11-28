c3f8f8298636f4a3ae0963fdb3e732cc0aebf8b1
Vinmay Patil <patilvinmay@gmail.com>
2023-11-28 18:03:26 +0530

    Update webscrap.py

--- BILayer/webscrap.py
+++ BILayer/webscrap.py
@@ -1,28 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import requests
+from bs4 import BeautifulSoup
 import time
 
-def wait_for_page_load(driver, timeout=10):
-    # Wait for the page to load by waiting for the presence of the body tag
-    WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.TAG_NAME, 'body'))
-    )
-
-def save_webpage_after_loading(url, output_filename):
+def save_webpage_with_selenium(url, output_filename):
     try:
-        # Initialize the Chrome driver
-        driver = webdriver.Chrome()
+        # Set up options for a headless Chrome browser
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(options=chrome_options)
 
         # Navigate to the webpage
         driver.get(url)
 
-        # Wait for the page to load (wait for the body tag to be present)
-        wait_for_page_load(driver)
-
-        # Wait for additional time (adjust as needed) to ensure dynamic content has loaded
+        # Wait for a moment (optional, adjust as needed)
         time.sleep(2)
 
         # Save the HTML content after the page has fully loaded
@@ -33,26 +26,42 @@ def save_webpage_after_loading(url, output_filename):
 
     except Exception as e:
         print(f"Error: {e}")
+
     finally:
         # Close the browser window
         driver.quit()
 
+def extract_rate_from_html(file_content):
+    try:
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(file_content, 'html.parser')
+
+        # Find the rate element
+        rate_element = soup.find('div', class_='variableRate')
+
+        # Extract and print the rate
+        if rate_element:
+            rate_fixed = rate_element.get_text().strip()
+            print(f"Fixed Rate: {rate_fixed}")
+        else:
+            print("Rate element not found in the HTML.")
+
+    except Exception as e:
+        print(f"Error extracting rate: {e}")
+
 if __name__ == "__main__":
-    # Replace 'https://example.com' with the URL of the webpage you want to open and save
+    # Replace 'https://example.com' with the URL of the webpage you want to download
     url = 'https://www.discover.com/student-loans/'
 
     # Replace 'output.html' with the desired filename for the saved content
     output_filename = 'C:\\Users\\vinmayp\\Desktop\\Resources\\output.html'
 
-    # Call the function to open the webpage, wait for it to load, and then save the HTML code
-    save_webpage_after_loading(url, output_filename)
+    # Call the function to open the webpage with a headless browser and save the HTML code
+    save_webpage_with_selenium(url, output_filename)
 
+    # Read the HTML content from the saved file
     with open(output_filename, 'r', encoding='utf-8') as file:
         file_content = file.read()
 
-    from bs4 import BeautifulSoup
-
-    soup = BeautifulSoup(file_content, 'html.parser')
-    rate_element = soup.find('div', class_='variableRate')
-    rate_fixed = rate_element.get_text().strip()
-    print(rate_fixed)
+    # Extract and print the rate from the HTML content
+    extract_rate_from_html(file_content)
