408fef959a2a4e81a10d7b9cc2f874fd0e188f30
Vinmay Patil <patilvinmay@gmail.com>
2024-02-13 19:21:02 +0530

    Updated file with print_abcdefg

--- Open_From_Diff_Directory.py
+++ Open_From_Diff_Directory.py
@@ -2,13 +2,13 @@ import os
 import sys
 
 
-def print_abcdefg_hi(name):
-    print_abcdefg(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
     sys.path.insert(1, 'C:\\Users\\admin\\Desktop')
 
-from Hello import print_abcdefg_hi
+from Hello import print_hi
 
-print_abcdefg_hi('Vinu')
+print_hi('Vinu')
