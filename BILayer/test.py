5ba59bf256b8846d8a475bae8e9a332f018dc515
Vinmay Patil <patilvinmay@gmail.com>
2024-02-13 19:35:04 +0530

    Update test.py

--- BILayer/test.py
+++ BILayer/test.py
@@ -1,20 +1,42 @@
-import pandas as pd
-from sqlalchemy import create_engine
+from github import Github
+import base64
 
-# Sample DataFrame
-data = {
-    'id': [1, 2, 3],
-    'name': ['Alice', 'Bob', 'Charlie']
-}
+# personal access token
+g = Github('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')
 
-df = pd.DataFrame(data)
+# username/repository
+repo = g.get_repo('patilvinmay/Test')
 
-# Replace placeholders with your PostgreSQL database credentials and connection details
-DATABASE_URI = "postgresql://username:password@hostname:port/database_name"
-engine = create_engine(DATABASE_URI)
+# Get contents of the repository
+contents = repo.get_contents("")
 
-# Define the table name
-table_name = 'your_table_name'
+# List to store file names containing the search string
+search_string = "print"
+matching_files = []
 
-# Write the DataFrame to the PostgreSQL table
-df.to_sql(table_name, engine, if_exists='replace', index=False)
+# Iterate through all files
+while contents:
+    file_content = contents.pop(0)
+
+    # Check if its python file, then replace
+    if file_content.type == "file" and str(str(str(file_content.path).split("/")[-1]).split(".")[-1]) == "py":
+        # Get file name
+        current_content = repo.get_contents(file_content.path)
+        # Decode file content
+        current_decoded_content = base64.b64decode(file_content.content).decode('utf-8')
+        # Check match string
+        if search_string in str(current_decoded_content):
+            matching_files.append(file_content.name)
+            # Replace old string with new
+            new_content = str(current_decoded_content).replace("OLD_WAREHOUSE", "NEW_WAREHOUSE")
+            commit_message = "Updated file with NEW_WAREHOUSE"
+            # Update file with New Warehouse
+            repo.update_file(file_content.path, commit_message, new_content, file_content.sha)
+    # Recursive
+    elif file_content.type == "dir":
+        contents.extend(repo.get_contents(file_content.path))
+
+# Print the file names containing the search string
+print("Files containing '{}' in their contents:".format(search_string))
+for file_name in matching_files:
+    print(file_name)
