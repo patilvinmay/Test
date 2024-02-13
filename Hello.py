48a5e123efe6834c2d919cb9e7dd58dda76b0cdc
Vinmay Patil <patilvinmay@gmail.com>
2024-02-13 16:09:02 +0530

    Updated file content

--- Hello.py
+++ Hello.py
@@ -1,6 +1,1 @@
-def print_hi(name):
-    print(f'Hi, {name}')
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+ZGVmIHByaW50X2hlbGxvKG5hbWUpOg0KICAgIHByaW50KGYnSGksIHtuYW1lfScpDQoNCg0KaWYgX19uYW1lX18gPT0gJ19fbWFpbl9fJzoNCiAgICBwcmludF9oZWxsbygnUHlDaGFybScpDQo=
